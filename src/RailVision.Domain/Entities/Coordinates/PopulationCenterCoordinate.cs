42f215d64c8a44cfe33e6be0966b0c85f24e40d0
nurlancreus <nurlancreus007@gmail.com>
2025-03-28 17:32:05 +0400

    Add PopulationCenter entity

---
+++ src/RailVision.Domain/Entities/Coordinates/PopulationCenterCoordinate.cs
@@ -0,0 +1,8 @@
+﻿namespace RailVision.Domain.Entities.Coordinates
+{
+    public class PopulationCenterCoordinate : Coordinate
+    {
+        public Guid PopulationCenterId { get; set; }
+        public PopulationCenter PopulationCenter { get; set; } = null!;
+    }
+}
---
+++ src/RailVision.Domain/Entities/PopulationCenter.cs
@@ -0,0 +1,13 @@
+﻿using RailVision.Domain.Entities.Base;
+using RailVision.Domain.Entities.Coordinates;
+
+namespace RailVision.Domain.Entities
+{
+    public class PopulationCenter : BaseEntity
+    {
+        public long ElementId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Population { get; set; }
+        public PopulationCenterCoordinate Coordinate { get; set; } = null!;
+    }
+}
